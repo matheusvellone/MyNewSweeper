802cb0541c0a4fc13244c979a46584b2985f615e
Matheus Peviani Vellone <matheusvellone@hotmail.com>
2015-12-26 18:47:55 -0200

    removed file utils.js

--- MyNewSweeper.js
+++ MyNewSweeper.js
@@ -239,3 +239,11 @@ function contaMinasVizinhas(x, y){
   }
   return n;
 }
+
+function randomIntFromInterval(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function selectCell(x, y){
+  return $('.cell[x="'+x+'"][y="'+y+'"]');
+}
