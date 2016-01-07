7000a3ebc28a2731a6a773e112102e49d5173c89
Matheus Peviani Vellone <matheusvellone@hotmail.com>
2016-01-07 02:19:09 -0200

    updated demo

--- test.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  $('#start').on('click', function(){
-    var mns = new MyNewSweeper({
-      numRows: $('#numRows').val(),
-      numColumns: $('#numColumns').val(),
-      numMines: $('#numMines').val()
-    });
-  });
-});
